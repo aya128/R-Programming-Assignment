3db262c3db303a9b6578457cd7ad535471a9add2
aya128 <65582088+aya128@users.noreply.github.com>
2020-07-10 00:51:51 -0400

    Add files via upload
    1st commit

--- R Programming Assignment.R
+++ R Programming Assignment.R
@@ -12,3 +12,14 @@ makeCacheMatrix <- function(x = matrix()) {
        getInverse = getInverse)
 }
 
+cacheSolve <- function(x, ...) {
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("getting cached data")
+    return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
+}
